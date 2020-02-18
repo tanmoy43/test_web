d7d95231b6f9f1151c75634cd83ab2a0aa5c15b5
Tanmoy Barua <tanmoybarua43@gmail.com>
2020-02-19 01:36:30 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,7 @@
+$(function () {
+    var parent = $("#shuffle");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+});
